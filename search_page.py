0bd713aafad08fa0f7044c4a95ad87b54e8ca413
Czarina Luna <czarinagarcialuna@gmail.com>
2022-04-23 20:28:01 -0400

    Update dataset used

--- search_page.py
+++ search_page.py
@@ -4,20 +4,20 @@ import spacy
 from tqdm import tqdm
 from rank_bm25 import BM25Okapi
 
-data = pd.read_csv('tiktok_data.csv')
+data = pd.read_csv('data/data.csv')
 
 nlp = spacy.load('en_core_web_sm')
-tok_text = []
+tokenized_text = [] 
 
-for doc in tqdm(nlp.pipe(data['nlp_text'].values, disable=['tagger', 'parser', 'ner'])):
-    tok = [t.text for t in doc if t.is_alpha]
-    tok_text.append(tok)
+for doc in tqdm(nlp.pipe(data['preprocessed_text'].fillna('').str.lower().values, disable=['tagger', 'parser', 'ner'])):
+    tokenized = [token.text for token in doc if token.is_alpha]
+    tokenized_text.append(tokenized)
 
-bm25 = BM25Okapi(tok_text)
+bm25 = BM25Okapi(tokenized_text)
 
-def search_video(query, n=3):
+def search_video(query, result=3):
     tok_query = query.lower().split(' ')
-    results = bm25.get_top_n(tok_query, data['file_path'], n)
+    results = bm25.get_top_n(tok_query, data['file_path'], result)
     
     results_list = [video for video in results]
     return results_list
